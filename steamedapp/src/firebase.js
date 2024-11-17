0b362b7f2c8389955918f3b26f237f76b3e9975a
kegrimes <kegrimes02@gmail.com>
2024-11-17 16:12:13 -0500

    Initialize emulators

--- steamedapp/src/firebase.js
+++ steamedapp/src/firebase.js
@@ -2,6 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+// Imports for emulators
+import { Auth } from 'firebase/auth';
+import { Firestore } from 'firebase/firestore';
+import { Function } from 'firebase/functions';
+
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD4JfnpMGf6YWFz3CltBhhYzyI4fRDTbuM",
   authDomain: "steameducation-b1b03.firebaseapp.com",
@@ -15,3 +21,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Emulators
+const auth = Auth(app);
+const firestore = Firestore(app);
+const functions = Function(app);
+
+if(window.location.hostname === 'localhost') {
+  console.log("Using Emulators...");
+  auth.useEmulator("localhost", 9099);
+  firestore.useEmulator("localhost", 8080);
+  functions.useEmulator("localhost", 5001);
+}
+
+export default app;
