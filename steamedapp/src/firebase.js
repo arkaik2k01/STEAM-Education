52e9f98403357b8a67fedc16be7861593d14e77e
kegrimes <kegrimes02@gmail.com>
2024-11-17 15:41:47 -0500

    Create firebase.js

---
+++ steamedapp/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4JfnpMGf6YWFz3CltBhhYzyI4fRDTbuM",
+  authDomain: "steameducation-b1b03.firebaseapp.com",
+  projectId: "steameducation-b1b03",
+  storageBucket: "steameducation-b1b03.firebasestorage.app",
+  messagingSenderId: "104577670307",
+  appId: "1:104577670307:web:5b82417067bb5b9ae63316",
+  measurementId: "G-200FHB1HCH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
