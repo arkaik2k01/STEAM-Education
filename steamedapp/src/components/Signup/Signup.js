792a51b45c4963f0ce047672b1992b0424eedd8c
kegrimes <kegrimes02@gmail.com>
2024-11-19 13:46:15 -0500

    Create signup and login APIs

---
+++ steamedapp/src/components/Signup/Signup.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { auth } from '../../firebase';
+import 'Signup.css';
+
+function SignUp() {
+
+}
+
+export default SignUp;
--- steamedapp/src/firebase.js
+++ steamedapp/src/firebase.js
@@ -1,10 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
-// Imports for emulators
-import { Auth } from 'firebase/auth';
-import { Firestore } from 'firebase/firestore';
+// Imports for emulators and built-in functions
+import { getAuth, Auth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signOut, updateProfile } from 'firebase/auth';
+import { Firestore, getFirestore } from 'firebase/firestore';
 import { Function } from 'firebase/functions';
 
 // Firebase configuration
@@ -18,20 +17,111 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
+// Initialize Firebase, Firestore, and Authentication
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProv = GoogleAuthProvider();
 
 // Initialize Emulators
-const auth = Auth(app);
+const authem = Auth(app);
 const firestore = Firestore(app);
 const functions = Function(app);
 
 if(window.location.hostname === 'localhost') {
   console.log("Using Emulators...");
-  auth.useEmulator("localhost", 9099);
+  authem.useEmulator("localhost", 9099);
   firestore.useEmulator("localhost", 8080);
   functions.useEmulator("localhost", 5001);
 }
 
+// Sign up with email and password
+const signupWithEmail = async (displayName, email, password) => {
+  try{
+    const wait = await createUserWithEmailAndPassword(auth, email, password);
+    const user = wait.user;
+
+    await updateProfile(user, {displayName: displayName});
+  }
+  catch(err){
+    console.log(err);
+    alert("Could not create an account with email and password!");
+  }
+}
+
+// Sign in with Google account
+const googleSignIn = async () => {
+  try{
+    const wait = await signInWithPopup(auth, googleProv);
+    const user = wait.user;
+    console.log('Email: ', user.email);
+  }
+  catch(err){
+    console.log(err);
+    alert("Could not sign in with Google account!");
+  }
+}
+
+// Login with email and password
+const loginWithEmail = async (email, password) => {
+  try{
+    const wait = await signInWithEmailAndPassword(auth, email, password);
+    const user = wait.user;
+    console.log('Email: ', user.email);
+  }
+  catch(err){
+    console.log(err);
+    alert("Could not login with email and password!");
+  }
+}
+
+// Send password reset to email
+const resetPassword = async (email) => {
+  try{
+    await sendPasswordResetEmail(auth, email);
+    console.log('Password reset sent to email: ', email);
+    alert("Password reset sent to email!");
+  }
+  catch(err){
+    console.log(err);
+    alert("Could not send password reset to email!");
+  }
+}
+
+// Send email verification 
+const verifyEmail = async (user) => {
+  try{
+    await sendEmailVerification(user);
+    console.log('Email verification sent to ', user.email);
+    alert('Email verification sent!');
+  }
+  catch(err){
+    console.log('Could not send email verification to ', user.email);
+    alert("Could not send email verification!");
+  }
+}
+
+// Logout
+const logout = async () => {
+  try{
+    signOut(auth);
+    console.log("Successful signout!");
+  }
+  catch(err){
+    console.log('Could not sign out!');
+    alert('Could not sign out!');
+  }
+}
+
+export {
+  auth,
+  db,
+  signupWithEmail,
+  googleSignIn,
+  loginWithEmail,
+  resetPassword,
+  verifyEmail,
+  logout
+};
 export default app;
