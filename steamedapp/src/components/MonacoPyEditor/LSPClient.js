46a125c0fefc592275ebac212a6fc251eb30fb4e
arkaik2k01 <80938348+arkaik2k01@users.noreply.github.com>
2024-12-22 12:57:44 -0500

    Code Editor Initial
    Initial code editor set up.
    
    TODO:
    - Test editor locally
    - Test conenction once server hosting is set up
    - Replace local host values with server URLs

---
+++ steamedapp/src/components/MonacoPyEditor/LSPClient.js
@@ -0,0 +1,40 @@
+import { MonacoLanguageClient, CloseAction, ErrorAction } from "monaco-languageclient";
+import { listen } from 'vscode-ws-jsonrpc';
+
+// Create web socket to communicate the Monaco Editor with the LSP back-end
+const createWebSocketTransport = (url) => {
+    const webSocket = new WebSocket(url);
+    return listen({
+        webSocket,
+        onConnection: (connection) => {
+            return { reader: connection, writer: connection};
+        },
+    });
+};
+
+
+// IMPORTANT: Notice languageServer is just a placeholder until back-end infrastructure is ready
+export const startLSP = (editor, languageServer = 'ws://localhost:8080') => {
+    editor.languages.register({ id: 'python'});
+
+    //Initiate connection and promise to return object bridge, used to communicate between LSP and Editor
+    return createWebSocketTransport(languageServer).then((bridge) => {
+        
+        const languageClient = new MonacoLanguageClient({
+            name: 'Python LSP',
+            clientOptions: {
+                documentSelector: ['python'],
+                errorHandler: {
+                    error: () => ErrorAction.Continue, // Note that if connection errors, editor will still be online
+                    closed: () => CloseAction.Restart,
+                },
+            },
+            connectionProvider: {
+                get: () => Promise.resolve(bridge),
+            },
+        });
+
+        languageClient.start();
+        return languageClient;
+    });
+};
