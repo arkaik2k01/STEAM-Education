e846e0a5c62225d3cbe73b79244b372f8a1c83d6
arkaik2k01 <80938348+arkaik2k01@users.noreply.github.com>
2025-01-08 11:43:15 -0500

    Editor 3.0 - Basic Python
    Even further simplified monaco editor, with just python highlighting.

--- steamedapp/src/components/MonacoPyEditor/MonPyEditor.js
+++ steamedapp/src/components/MonacoPyEditor/MonPyEditor.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { listen, createMessageConnection } from 'vscode-ws-jsonrpc';
 import { Monaco } from '@monaco-editor/react';
-import { createWebSocket, configLSP } from './LSPClient';
-import { pyRegister } from './PyFallback';
 
-function MonacoPythonEditor({ LSPurl }) {
+function MonacoPythonEditor() {
     const [defaultValue, setDefaultValue] = useState('# Loading module . . .');
     const editorRef = useRef(null);
 
@@ -19,7 +16,9 @@ function MonacoPythonEditor({ LSPurl }) {
             setDefaultValue(data.module || '# A connection was established, but invalid or NULL data was received from server.'); // Switch this part depending on end point return formatting
         } catch (error) {
             console.error('There was a error fetching module content:', error);
-            setDefaultValue('# Fetching module info failed. A error occurred when communicating with the back end.'); // In case of error, editor will display error message
+            setDefaultValue(`# Fetching module info failed. 
+                An error occurred when communicating with the server, 
+                or incorrect data was received.\n${error}`); // In case of error, editor will display error message
         }
     }
 
@@ -27,26 +26,7 @@ function MonacoPythonEditor({ LSPurl }) {
     useEffect(() => {
         //Fetch module and load from back end
         fetchModuleContent();
-
-        //Establish a connection with the back end LSP
-        if (LSPurl) {
-            const url = `ws://${LSPurl}`;
-            const webSocket = createWebSocket(url);
-
-            // Connect WebSocket to LSP
-            listen({
-                webSocket,
-                onConnection: (connection) => {
-                    const transports = createMessageConnection(connection);
-                    const langClient = configLSP(transports);
-                    langClient.start();
-                    connection.onClose(() => langClient.stop())
-                },
-            });
-        } else {
-            console.warn('A connection with the language server was not able to be established. Switching to basic Python support.');
-        }
-    }, [LSPurl]);
+    }, []);
 }
 
 return (
@@ -56,9 +36,11 @@ return (
         defaultLanguage="python"
         language="python"
         value={defaultValue} //Fetched value
-        onMount={(editorRef, monaco) => {
-            editorRef.current = editor
-            pyRegister(monaco);
+        onMount={(editor, monaco) => {
+            editorRef.current = editor;
+            console.log('Mounting editor . . .');
         }}
     />
 );
+
+export default MonacoPythonEditor;
